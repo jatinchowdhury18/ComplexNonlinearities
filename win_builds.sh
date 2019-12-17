69081b51b186c4c97d5da252c3b521b32bb1bc6d
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-12-17 14:41:04 -0700

    Add Windows builds

---
+++ win_builds.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export PATH="/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/":$PATH
+
+for Dir in `find . -maxdepth 1 -type d`
+do
+    if [ -d $Dir/Plugin ]
+        then
+        name="${Dir:2}"
+
+        echo "Building $name..."
+        cd $Dir/Plugin/Builds/VisualStudio2017/
+        # MSBuild.exe -v:quiet -t:rebuild -p:Configuration=RELEASE -p:Platform=x64 $name.sln
+
+        echo "Copying $name ..."
+        cd ../../../../
+        cp "$Dir/Plugin/Builds/VisualStudio2017/x64/Release/Standalone Plugin/$name.exe" Bin/$name/Win # standalone
+        cp "$Dir/Plugin/Builds/VisualStudio2017/x64/Release/VST/$name.dll" Bin/$name/Win # VST
+        cp "$Dir/Plugin/Builds/VisualStudio2017/x64/Release/VST3/$name.vst3" Bin/$name/Win # VST3
+    fi
+done
