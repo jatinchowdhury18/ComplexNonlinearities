a511eb775f16a965238fe1de23907e82ecef2de5
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-03-02 17:18:51 -0800

    Add presentation materials

--- NonlinearBiquad/nlBiquad.py
+++ NonlinearBiquad/nlBiquad.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.signal as signal
 import matplotlib.pyplot as plt
 import FilterShapes as filters
+plt.style.use('dark_background')
 
 #%%
 def chirpLog (f0, f1, duration, fs):
@@ -42,7 +43,7 @@ class Biquad:
     def processSample (self, x):
         y = self.z[1] + self.b[0]*x
         self.z[1] = self.saturator (self.z[2] + self.b[1]*x - self.a[1]*y)
-        self.z[2] = self.saturator (self.b[2]*x - self.a[2]*y))
+        self.z[2] = self.saturator (self.b[2]*x - self.a[2]*y)
         return y
 
     def processBlock (self, block):
@@ -68,7 +69,7 @@ def softClip (x):
 
 nlBQ = Biquad()
 nlBQ.setCoefs (b, a)
-nlBQ.saturator = lambda x : np.tanh (x)
+nlBQ.saturator = lambda x : 0.43*np.abs(x) # np.tanh (x)
 # nlBQ.bLambdas[0] = lambda x, b : np.tanh (x) * b
 # nlBQ.bLambdas[1] = lambda x, b : np.tanh (x) * b
 # nlBQ.bLambdas[2] = lambda x, b : np.tanh (x) * b
@@ -79,11 +80,11 @@ x_0 = 10
 x = np.ones (N) * x_0
 y = nlBQ.processBlock (x)
 
-plt.plot (y)
-print (y[N-1])
+# plt.plot (y)
+# print (y[N-1])
 
-guess = (-1.0 / nlBQ.a[2]) * (1.0 - nlBQ.b[2] * x_0)
-print (guess)
+# guess = (-1.0 / nlBQ.a[2]) * (1.0 - nlBQ.b[2] * x_0)
+# print (guess)
 
 #%%
 def plotFilterResponse (biquad, fs, gain=0.1):
@@ -91,11 +92,13 @@ def plotFilterResponse (biquad, fs, gain=0.1):
     y = biquad.processBlock (np.copy (x))
     N = len (x)
 
-    h = chrp2ir (x, y)
+    plt.plot(np.arange(N)/fs, y)
 
-    f = np.linspace (0, fs/2, num=N/2+1)
-    H = np.fft.rfft (h)
-    plt.semilogx (f, 20 * np.log10 (np.abs (H)))
+    # h = chrp2ir (x, y)
+
+    # f = np.linspace (0, fs/2, num=N//2+1)
+    # H = np.fft.rfft (h)
+    # plt.semilogx (f, 20 * np.log10 (np.abs (H)))
 
 def plotStaticCurve (biquad, gain=0.1):
     x = gain*chirpLog (20, 20000, 1, fs)
@@ -105,11 +108,12 @@ def plotStaticCurve (biquad, gain=0.1):
 plt.figure()
 plotFilterResponse (normalBQ, fs)
 plotFilterResponse (nlBQ, fs)
-plt.xlim (20, 20000)
+# plt.xlim (20, 20000)
+# plt.ylim(-30)
 
-plt.title ('Nonlinear Low Shelf')
-plt.xlabel ('Frequency [Hz]')
-plt.ylabel ('Magnitude [dB]')
+plt.title ('Nonlinear Lowpass Sine Sweep (abs)')
+plt.xlabel ('Time [Seconds]')
+plt.ylabel ('Magnitude')
 plt.legend (['Linear', 'Nonlinear'])
 
 # plt.ylim (-10, 10)
@@ -119,31 +123,32 @@ plt.legend (['Linear', 'Nonlinear'])
 # plt.ylim (-20)
 
 #%%
-N = 1000
-x = np.linspace (-3, 3, N)
-y = np.copy (x)
+# N = 1000
+# x = np.linspace (-3, 3, N)
+# y = np.copy (x)
 
-def tanhClip (x, sat):
-    return sat * np.tanh (x / sat)
+# def tanhClip (x, sat):
+#     return sat * np.tanh (x / sat)
 
-def softClip (x,  sat):
-    if isinstance (x, np.ndarray):
-        t = np.copy (x)
-        for n in range (len (x)):
-            t[n] = softClip (t[n], sat)
-        return t
+# def softClip (x,  sat):
+#     if isinstance (x, np.ndarray):
+#         t = np.copy (x)
+#         for n in range (len (x)):
+#             t[n] = softClip (t[n], sat)
+#         return t
     
-    if (x > 1.5): return 1
-    if (x < -1.5): return -1
-    x /= 1.5
-    return (x - x*x*x/3)
+#     if (x > 1.5): return 1
+#     if (x < -1.5): return -1
+#     x /= 1.5
+#     return (x - x*x*x/3)
 
-plt.plot (x, y)
-plt.plot (x, tanhClip (x, 1), color='red')
-plt.plot (x, tanhClip (x, 3), color='red')
+# plt.plot (x, y)
+# plt.plot (x, tanhClip (x, 1), color='red')
+# plt.plot (x, tanhClip (x, 3), color='red')
 
-plt.plot (x, softClip (x, 1), color='skyblue')
+# plt.plot (x, softClip (x, 1), color='skyblue')
 
 
 
 #%%
+plt.show()
